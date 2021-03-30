a2150acf187fa1647a9a133092f763f8e310ea00
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-03-31 01:13:01 +0300

    Initial commit

---
+++ app/src/main/java/group/moveon/constraintlayoutworkingout/DetailsActivity.java
@@ -0,0 +1,14 @@
+package group.moveon.constraintlayoutworkingout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/group/moveon/constraintlayoutworkingout/MainActivity.java
@@ -0,0 +1,14 @@
+package group.moveon.constraintlayoutworkingout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
