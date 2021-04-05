4689a7dd5af92a68ea14e7097a64ce2344ae2455
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-04-05 03:16:23 +0300

    Final commit

--- app/src/main/java/group/moveon/constraintlayoutworkingout/MainActivity.java
+++ app/src/main/java/group/moveon/constraintlayoutworkingout/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        setContentView(R.layout.details_activity);
     }
 }
